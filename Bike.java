54a57d3ce42f017c3d2b96382442c6130ae2951f
ddkeen <d3keen@gmail.com>
2021-02-09 11:02:34 -0500

    indentation

--- Bike.java
+++ Bike.java
@@ -12,49 +12,49 @@ public abstract class Bike {
     protected boolean hasPedals;
     protected boolean hasTrainingWheels;
 
-	/**
+    /**
      * Creates a bike by adding a frame, wheels, pedals, and a price
      */
-	public void createBike() {
+    public void createBike() {
         createFrame();
         addWheels();
         addPedals();
         getPrice();
-	}
+    }
     
     /**
      * Prints a representation of the frame being built for the particular bike
      */
-	private void createFrame() {
-		System.out.println("Assembling " + name + " frame");
-	}
+    private void createFrame() {
+        System.out.println("Assembling " + name + " frame");
+    }
     
     /**
      * Prints a representation of the number of wheels and training wheels being added for the particular bike if it has them
      */
-	private void addWheels() {
+    private void addWheels() {
         if(numWheels != 0) {
             System.out.println("Adding " + numWheels + " wheel(s)");
             if(hasTrainingWheels) {
                 System.out.println("Adding training wheels");
             }
         }
-	}
+    }
     
     /**
      * Prints a representation of the pedals being added for the particular bike if it has them
      */
-	private void addPedals() {
+    private void addPedals() {
         if(hasPedals) {
             System.out.println("Adding pedals");
         }
-	}
+    }
     
     /**
      * Prints a representation of the price of the particular bike
      */
-	public void getPrice() {
-		System.out.println("Price: $" + price);
-	}
+    public void getPrice() {
+	    System.out.println("Price: $" + price);
+    }
     
 }
