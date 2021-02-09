584b4bf8383ab2e3807d22bf2e3d54ddae487753
ddkeen <d3keen@gmail.com>
2021-02-09 11:04:28 -0500

    more spacing

--- Strider.java
+++ Strider.java
@@ -8,7 +8,6 @@ public class Strider extends Bike {
      * Strider contructor that sets its name, price, number of wheels, and whether it has pedals and/or training wheels
      */
     public Strider() {
-
         name = "Strider";
         price = 65.99;
         numWheels = 2;
